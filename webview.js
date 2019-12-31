7c4df5dee65dbb914af32edca26d20f664998c7d
ahaenggli <adriano@haenggli.net>
2019-12-31 11:16:32 +0100

    split msgCount and privateMsgCount

--- webview.js
+++ webview.js
@@ -1,13 +1,32 @@
 module.exports = (Franz) => {
-	const getMessages = function getMessages() {		
+	const getMessages = function getMessages() {	
+		
+		var msgCount = 0;
+		var privateMsgCount = 0;
+
+		// Total 
 		const $NumberInDocTitle = document.title.match(/\(([^)]+)\)/);
-		msgCount = 0;
 		if ( $NumberInDocTitle!=null && $NumberInDocTitle.length > 0 ) {
 			msgCount = parseInt($NumberInDocTitle[1]);
-			if(msgCount == null || isNaN(msgCount)) msgCount = 0;
-			
+			if(msgCount == null || isNaN(msgCount)) msgCount = 0;			
 		} 
-		Franz.setBadge( msgCount );
+		
+		// just private messages
+		var linkPriv = [].filter.call(document.getElementsByTagName('a'), el => el.href.indexOf('Messages') >= 0);
+		if(linkPriv.length >= 1){
+			linkPriv = linkPriv[0].getElementsByClassName('q-badge');
+			if(linkPriv.length >= 1){
+				linkPriv = linkPriv[0].innerHTML;
+				
+				if ( linkPriv!=null && linkPriv.length > 0 ) {
+					privateMsgCount = parseInt(linkPriv);
+					if(privateMsgCount == null || isNaN(privateMsgCount)) privateMsgCount = 0;					
+				} 
+			}			
+		}
+		
+		msgCount = msgCount - privateMsgCount;
+		Franz.setBadge(privateMsgCount, msgCount);
 	};
   
 	// Check for new messages every second and update Franz badge.
